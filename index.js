cc99b06ffc24c4ded787373120eccf3b53041e28
Gauravsingh <singhgaurav.rewamp@gmail.com>
2024-06-15 18:01:22 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+
+var data=0;
+function increment(){
+    var p1= document.querySelector("#counting").innerHTML=data;
+    data++
+}
+
+function decrement(){
+    var p1= document.querySelector("#counting").innerHTML=data;
+    data--
+}
